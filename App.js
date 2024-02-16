93f5abd1f76f510bfcc1b83f2fd42e73fd524c7b
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-16 21:49:33 +0600

    total cost validation complete

--- App.js
+++ App.js
@@ -18,11 +18,28 @@ for (let index = 0; index < addSelectBtn.length; index++) {
         addElementId('cart-count', count);
 
 
-        // total cost function
+        // place and price name
         const placeName = event.target.parentNode.childNodes[1].innerText;
-        const price = parseInt(event.target.parentNode.childNodes[3].childNodes[1].innerText);
-        const priceCost = document.getElementById('total-cost').innerText;
-        
+        const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+
+        // cart add element
+        const cartAdd = document.getElementById('selected-place-container');
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerText = placeName;
+        const p2 = document.createElement('p');
+        p2.innerText = price;
+        li.appendChild(p);
+        li.appendChild(p2);
+        cartAdd.appendChild(li);
+
+        // total cost 
+
+        const totalCost  = document.getElementById('total-cost').innerText;
+        const convertTotalCost = parseInt(totalCost);
+        const sum = convertTotalCost + parseInt(price);
+        addElementId('total-cost', sum);
+
 
         // addElementId('total-cost', priceCost);
     })
